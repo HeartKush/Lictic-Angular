503c1eae4b9b7fff41ec226956c2562fba8583f8
Andres Parra <andresparra@gmail.com>
2022-09-20 19:59:33 -0500

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Lictic-Angular';
+  ngOnInit() {
+    // Set the date we're counting down to
+    var countDownDate = new Date("Dec 3, 2022 18:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      /*  document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+           minutes + "m " + seconds + "s "; */
+
+      document.getElementById("days")!.innerHTML = days + "d ";
+      document.getElementById("hours")!.innerHTML = hours + "h ";
+      document.getElementById("minutes")!.innerHTML = minutes + "m ";
+      document.getElementById("seconds")!.innerHTML = seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo")!.innerHTML = "EXPIRED";
+      }
+    }, 1000);
+
+  }
 }
+
+
